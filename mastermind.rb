03a34af4e453a23ce55c2835e949c23d986143d8
Adam Halfpenny <adamhalfpenny@gmail.com>
2020-04-17 16:35:04 -0400

    added basic guess comparison logic

--- mastermind.rb
+++ mastermind.rb
@@ -11,12 +11,27 @@ class Game
         valid_digits = Array(1..6)
         puts "Please enter your guess seperated by commas (eg 1,1,1,1)"
         code_guess = gets.chomp.split(",")
-        if code_guess.length == 4 && code_guess.all?{ |digit| valid_digits.include?(digit.to_i)}
-            puts code_guess.join(',')
+        code_guess.map! {|x| x = x.to_i}
+        if code_guess.length == 4 && code_guess.all?{ |digit| valid_digits.include?(digit)}
+            compare_guess(code_guess)
         else
             puts "Please enter a valid unput!"
             self.guess
         end
+    end
+
+    def compare_guess(guess)
+        puts "comparing guess: #{guess.join(",")}"
+
+        guess.each_with_index do |e, i|
+            if  @@secret_code.include?(e)
+                puts "Included"
+                if @@secret_code[i] == e
+                    puts "Correct location!"
+                end
+            end
+        end
+
     end
 end
 
