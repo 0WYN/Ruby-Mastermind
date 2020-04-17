afdb874eef9dc199ed780d752da21e4780632bce
Adam Halfpenny <adamhalfpenny@gmail.com>
2020-04-17 13:43:35 -0400

    Added guess method including guess validation checks

--- mastermind.rb
+++ mastermind.rb
@@ -6,6 +6,19 @@ class Game
         end
         puts @@secret_code.join(",")
     end
+
+    def guess
+        valid_digits = Array(1..6)
+        puts "Please enter your guess seperated by commas (eg 1,1,1,1)"
+        code_guess = gets.chomp.split(",")
+        if code_guess.length == 4 && code_guess.all?{ |digit| valid_digits.include?(digit.to_i)}
+            puts code_guess.join(',')
+        else
+            puts "Please enter a valid unput!"
+            self.guess
+        end
+    end
 end
 
 game = Game.new
+game.guess
